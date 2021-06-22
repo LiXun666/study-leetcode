c84f46eb45e090872fd1443e05d677d622463b56
lxjj <754975952@qq.com>
2021-06-22 17:46:40 +0800

    lc1281

---
+++ src/main/java/leetcode/LC1281.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * @Author lxjj
+ * @Date 2021-06-22 17:35
+ * @Description 整数的各位积和之差
+ */
+public class LC1281 {
+    public int subtractProductAndSum(int n) {
+        int sum = 0;
+        int product = 1;
+        while (n != 0) {
+            int temp = n % 10;
+            sum += temp;
+            product *= temp;
+
+            n /= 10;
+        }
+        return product - sum;
+    }
+}
